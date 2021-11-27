496c2394401641ea32f108c06560ff1fc1c47c02
pedro <pedropmancillac@gmail.com>
2021-11-27 10:40:28 -0300

    migracion

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_122830) do
+ActiveRecord::Schema.define(version: 2021_11_27_133423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lead_talks", force: :cascade do |t|
+    t.bigint "lead_id", null: false
+    t.bigint "talk_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lead_id"], name: "index_lead_talks_on_lead_id"
+    t.index ["talk_id"], name: "index_lead_talks_on_talk_id"
+    t.index ["user_id"], name: "index_lead_talks_on_user_id"
+  end
+
+  create_table "leads", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "telephone"
+    t.string "email"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_leads_on_user_id"
+  end
+
+  create_table "talks", force: :cascade do |t|
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +56,8 @@ ActiveRecord::Schema.define(version: 2021_11_27_122830) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lead_talks", "leads"
+  add_foreign_key "lead_talks", "talks"
+  add_foreign_key "lead_talks", "users"
+  add_foreign_key "leads", "users"
 end
